70f01008a4b56a57a9af297e2caf0e2f2f593a5b
ruddypp <paningalrudy@gmail.com>
2026-01-21 21:33:05 +0700

    remove react

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { X } from 'lucide-react';
