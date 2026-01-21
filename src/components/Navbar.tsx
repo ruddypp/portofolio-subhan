85dd20a6bd0a1d7330bb774773248ca4025f49bb
ruddypp <paningalrudy@gmail.com>
2026-01-21 21:40:27 +0700

    fix responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -27,7 +27,7 @@ const Navbar = () => {
     return (
         <nav className="fixed w-full top-0 z-50">
             {/* Header Content with Mix Blend */}
-            <div className="w-full px-6 py-6 md:px-10 md:py-6 flex justify-between items-center mix-blend-difference text-[var(--color-paper)]">
+            <div className="relative z-50 w-full px-6 py-6 md:px-10 md:py-6 flex justify-between items-center mix-blend-difference text-[var(--color-paper)]">
                 <div className="flex flex-col items-start leading-none nav-item">
                     <span className="text-lg font-bold tracking-widest uppercase mb-0.5">Ahmad Subhan</span>
                     <span className="text-[0.6rem] tracking-[0.2em] uppercase opacity-80">Fotografer</span>
